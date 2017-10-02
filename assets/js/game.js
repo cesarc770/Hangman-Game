f166515d24c7fda7af7eb0708f6b7afb630e9cfa
Cesar Caceres <cesarc770@gmail.com>
2017-10-02 15:47:53 -0600

    added functions to obtain userInput

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,15 @@
-// an object to store words, choose a word
+// create an object game to store words, choose a word  tries
 
 var game = {
 	words : ["referee", "ronaldinho", "goal"],
+	tries : 10,
 	chosenWord : function(){ return this.words[Math.floor(Math.random() * this.words.length)];},
 
 }
 
+//create function to display word in spaces
 var chosenWord = game.chosenWord();
-//display word using array of spaces
+
 function displayWord(chosenWord){ 
 	var str = [];
 	for(var i = 0; i < chosenWord.length; i++){
@@ -15,5 +17,16 @@ function displayWord(chosenWord){
 	}
 		return str;
 	}
+
+var str  = displayWord(chosenWord);
 console.log(chosenWord);
 console.log(str);
+
+//obtain input from user and save it in a variable
+
+document.onkeyup = function(event){
+	var userInput = event.key;
+
+//everything that will happen when the key comes up goes in here
+
+}
