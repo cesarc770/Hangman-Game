04d0bad29272d6c7b79e5d427cae19c289e1cd2f
Cesar Caceres <cesarc770@gmail.com>
2017-10-02 18:39:04 -0600

    added words to the used letters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,5 @@
+
+
 // create an object game to store words, choose a word  tries
 
 var game = {
@@ -7,8 +9,13 @@ var game = {
 
 }
 
+//get element from html
+var spaces = document.getElementById("spaces");
+var guessed = document.getElementById("guessed")
+
 //create function to display word in spaces
 var chosenWord = game.chosenWord();
+var tries = game.tries;
 
 function displayWord(chosenWord){ 
 	var str = [];
@@ -23,18 +30,35 @@ var str  = displayWord(chosenWord);
 console.log(chosenWord);
 console.log(str);
 
+//function to reduce tries --- not working yet / not being used
+function reduceTries(){
+	tries = tries - 1;
+	return tries;
+}
+
+//function to display letters being input
+function inputLetters(userInput){
+
+	if(guessed.innerHTML.indexOf(userInput) === -1){
+		guessed.innerHTML += userInput.toUpperCase()+ " ";
+		
+	}
+	
+}
+
 //Function to compare userInput to word and store it in an array
 
 function compare(userInput, str, chosenWord) {
 	for(var i = 0; i < chosenWord.length; i++){
 		if(userInput === chosenWord[i]){
-			str[i] = userInput;
+			str[i] = userInput.toUpperCase();
 		}
+
 	} return str;
 
 }
 
-
+spaces.innerHTML = str.join("");
 
 
 //obtain input from user and save it in a variable
@@ -44,9 +68,16 @@ document.onkeyup = function(event){
 
 //everything that will happen when the key comes up goes in here
 
+//
+
+//add letter to guessed letters 
+inputLetters(userInput);
+
 //compare userInput and the chosenWord and add to space str
 console.log(compare(userInput, str, chosenWord));
 
-
+spaces.innerHTML = str.join("");
 
 }
+
+
